2bef2486528f78750e190d69b5182ec39a118b1e
VS <hsouhane1@gmail.com>
2024-08-16 15:54:31 +0100

    Model up

--- cs-back/src/models/Commande.js
+++ cs-back/src/models/Commande.js
@@ -8,6 +8,7 @@ const Commande = new mongoose.Schema(
     city: { type: String, required: false, default: "" },
     product: { type: String, required: false, default: "" },
     adresse: { type: String, required: false, default: "" },
+    tel: { type: String, required: false, default: "" },
     status: { type: String, required: false, default: "PENDIND" },
     role: { type: String, required: false, default: "CLIENT" },
   },
