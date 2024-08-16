091cc9ed3c20818e77e026db404f887e03b924f4
VS <hsouhane1@gmail.com>
2024-08-16 20:41:10 +0100

    p to g

--- cs-back/src/routes/commande.route.js
+++ cs-back/src/routes/commande.route.js
@@ -3,6 +3,6 @@ const { commandeController } = require("../controllers");
 const router = require("express").Router();
 
 router.post("/add", commandeController.alterCommande);
-router.post("/all", commandeController.getAllCommandes);
+router.get("/all", commandeController.getAllCommandes);
 
 module.exports = router;
